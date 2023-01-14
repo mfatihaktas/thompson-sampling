3c4cb16438654000370d0d778404f57f50751831
Mehmet Aktas <mfatihaktas@gmail.com>
2023-01-14 18:12:23 -0500

    Added pull_high_reward() and mean_high_reward() in Bandit

--- src/env/bandit.py
+++ src/env/bandit.py
@@ -1,7 +1,8 @@
 import abc
 
-from src.prob import rv
 from src.env import arm as arm_module
+from src.prob import rv
+from src.utils.debug import *
 
 
 class Bandit:
@@ -12,26 +13,39 @@ class Bandit:
     def pull(self, arm_index):
         pass
 
+    @abc.abstractmethod
+    def pull_high_reward(self):
+        pass
+
+    @abc.abstractmethod
+    def mean_high_reward(self):
+        pass
+
 
 class StationaryBandit(Bandit):
     def __init__(
         self,
         num_arms: int,
+        num_arms_w_high_reward: int,
         high_reward_rv: rv.RandomVariable,
         low_reward_rv: rv.RandomVariable,
     ):
         self.num_arms = num_arms
+        self.num_arms_w_high_reward = num_arms_w_high_reward
         self.high_reward_rv = high_reward_rv
         self.low_reward_rv = low_reward_rv
 
+        check(self.num_arms_w_high_reward <= self.num_arms, "")
+
         self.arm_list = [
             arm_module.StationaryArm(
-                name=f"high_reward_arm",
+                name=f"high_reward_arm_{i}",
                 reward_rv=self.high_reward_rv,
             )
+            for i in range(self.num_arms_w_high_reward)
         ]
 
-        for i in range(num_arms - 1):
+        for i in range(self.num_arms - self.num_arms_w_high_reward - 1):
             self.arm_list.append(
                 arm_module.StationaryArm(
                     name=f"low_reward_arm_{i}",
@@ -43,6 +57,7 @@ class StationaryBandit(Bandit):
         return (
             "StationaryBandit( \n"
             f"\t num_arms= {self.num_arms} \n"
+            f"\t num_arms_w_high_reward= {self.num_arms_w_high_reward} \n"
             f"\t high_reward_rv= {self.high_reward_rv} \n"
             f"\t low_reward_rv= {self.low_reward_rv} \n"
             ")"
@@ -50,3 +65,10 @@ class StationaryBandit(Bandit):
 
     def pull(self, arm_index: int):
         return self.arm_list[arm_index].pull()
+
+    def pull_high_reward(self):
+        # Note: High-reward arms are at the beginning of `arm_list`.
+        return self.arm_list[0].pull()
+
+    def mean_high_reward(self):
+        return self.high_reward_rv.mean()
